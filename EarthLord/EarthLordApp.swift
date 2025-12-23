4f397f77b7aa72ef558ace3d72fd26fb55cffa95
zhouxiaohong <xiaohongzhou730@gmail.com>
2025-12-23 21:12:10 +0800

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by 周晓红 on 2025/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
